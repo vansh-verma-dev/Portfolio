2fc4e8e057689ff0308f4564ff047430e4ee7126
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-11 18:51:18 +0530

    update webpage

---
+++ home.js
@@ -0,0 +1,4 @@
+const projectcard = document.getElementById('projectbtn');
+const seeproject = document.getElementById('seeproject').addEventListener('click',()=>{
+    window.location = projectcard;
+})
