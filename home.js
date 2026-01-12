ed386899c985b29333cf4b96bfb1fa108ad3d8be
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-12 09:10:52 +0530

    Final portfolio release

--- home.js
+++ home.js
@@ -1,4 +1,1 @@
-const projectcard = document.getElementById('projectbtn');
-const seeproject = document.getElementById('seeproject').addEventListener('click',()=>{
-    window.location = projectcard;
-})
+ 
