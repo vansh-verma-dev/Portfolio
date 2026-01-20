bc6cf4f06ed20bb3545ec2cea02767d1f997f601
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-20 23:41:36 +0530

    initial contact form logic

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
  const input = document.getElementById('input');
  const email = document.getElementById('email');
  const msg = document.getElementById('msg');
+
+ console.log('create by vansh verma');
