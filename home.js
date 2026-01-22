7a975fbe6dc9c2b6b060c2b6fd1559d1af0995c9
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-22 23:01:14 +0530

    initial resume button logic

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
  const input = document.getElementById('input');
  const email = document.getElementById('email');
  const msg = document.getElementById('msg');
-
- console.log('create by vansh verma');
+ 
