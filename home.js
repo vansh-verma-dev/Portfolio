dbf2cfa47be86b6a1b19ae98da5d055a9bd570a1
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-23 19:45:43 +0530

    initial add new file manifest.json & se.js

--- home.js
+++ home.js
@@ -1,4 +1,29 @@
- const input = document.getElementById('input');
- const email = document.getElementById('email');
- const msg = document.getElementById('msg');
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/Portfolio/sw.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.log('SW registration failed:', err));
+  });
+}
+
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.hidden = false;
+});
+
+installBtn.addEventListener("click", async () => {
+  installBtn.hidden = true;
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    deferredPrompt = null;
+    console.log("Install result:", outcome);
+  }
+});
+
+
  
